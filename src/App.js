427192b6d6a0391892a53a154f1ce46ccf65b97f
Kenny <kenny@knnyczr.com>
2023-01-29 18:07:16 -0500

    adding new styles

--- src/App.js
+++ src/App.js
@@ -20,14 +20,15 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(e.target[0].value);
-    setFoods((prevState) => [
-      ...prevState,
-      { name: e.target[0].value, type: e.target[1].type },
-    ]);
+    if (e.target[0].value !== "") {
+      setFoods((prevState) => [
+        ...prevState,
+        { name: e.target[0].value, type: e.target[1].type },
+      ]);
+    }
   };
 
   const handlePick = () => {
-    console.log("clicked!");
     let randomNumIndex = Math.floor(Math.random() * foods.length);
     setPicked(foods[randomNumIndex]);
   };
@@ -50,16 +51,27 @@ function App() {
         ))}
       </div>
 
-      <form className="py-4 w-100" onSubmit={(e) => handleSubmit(e)}>
-        <input placeholder="name" name="title" type="text"></input>
-        <select name="type">
-          <option>Restaurant</option>
-          <option>Food</option>
-        </select>
-        <button type="submit">➕</button>
-      </form>
+      <div className="min-w-full flex flex-col items-center">
+        <form className="py-4 w-100" onSubmit={(e) => handleSubmit(e)}>
+          <input
+            className="p-4"
+            placeholder="name"
+            name="title"
+            type="text"
+          ></input>
+          <select className="p-4" name="type">
+            <option>Restaurant</option>
+            <option>Food</option>
+          </select>
+          <button className="ml-10 p-4" type="submit">
+            ➕
+          </button>
+        </form>
+      </div>
 
-      <PickButton handlePick={handlePick} />
+      <div className="mt-20 min-w-full flex flex-col items-center">
+        <PickButton handlePick={handlePick} />
+      </div>
     </div>
   );
 }
--- src/components/PickButton.js
+++ src/components/PickButton.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function PickButton({ handlePick }) {
   return (
     <button
-      className="bg-gradient-radial from-purple-600 uppercase font-semibold text-lg bg-orange-400 rounded-full p-16"
+      className=" uppercase font-semibold text-lg bg-orange-400 rounded-full p-16"
       onClick={() => handlePick()}
     >
       Pick!
